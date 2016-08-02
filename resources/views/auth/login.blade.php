b6cdc4f63b617df564be1c5bd3100df86ee2159a
GlenWinWin <glenwinbernabe@gmail.com>
2016-08-02 14:04:00 +0800

    modified login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,11 +50,11 @@
                                               <form role="form" action="" method="post" class="login-form">
                                                 <div class="form-group">
                                                   <label class="sr-only" for="form-username">Username</label>
-                                                    <input type="text" name="form-username" placeholder="Username..." class="form-username form-control" id="form-username">
+                                                    <input type="text" name="username" placeholder="Username..." class="form-username form-control" id="form-username">
                                                   </div>
                                                   <div class="form-group">
                                                     <label class="sr-only" for="form-password">Password</label>
-                                                    <input type="password" name="form-password" placeholder="Password..." class="form-password form-control" id="form-password">
+                                                    <input type="password" name="password" placeholder="Password..." class="form-password form-control" id="form-password">
                                                   </div>
                                                   <center><button type="submit" class="btn">Log-in</button></center>
                                               </form>
