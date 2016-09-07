cec7efbc19c09f5593cc80b41f12954a8a8310d1
Jeysown <jeysownlopez@gmail.com>
2016-09-07 16:00:29 +0800

    genealogy size

--- resources/views/distributor/genealogy.blade.php
+++ resources/views/distributor/genealogy.blade.php
@@ -7,29 +7,34 @@ Genealogy
   <div class="col-lg-9">
     <center>
       <div class="col-xs-12 genealogy-main">
-        <img src="{{ Auth::user()->profile_path }}" alt="" style="width:20%;padding-top:20px;"/>
+        <img src="{{ Auth::user()->profile_path }}" alt="" style="padding-top:20px;"/>
         <div class="vertical_line"></div>
         <hr class="hr-genealogy" style="margin-bottom:0px;">
     </div>
         <div class="col-xs-15" >
           <i class="fa fa-arrow-down" aria-hidden="true" style="margin-left:22px;"></i>
-          <img src="{{$image1}}" alt="" style="width:100%;"/>
+          <br>
+          <img src="{{$image1}}" alt=""/>
           </div>
           <div class="col-xs-15" >
             <i class="fa fa-arrow-down" aria-hidden="true"></i>
-            <img src="{{$image2}}" alt="" style="width:100%;"/>
+            <br>
+            <img src="{{$image2}}" alt="" />
             </div>
             <div class="col-xs-15">
               <i class="fa fa-arrow-down" aria-hidden="true" style="margin-right:5px;"></i>
-              <img src="{{$image3}}" alt="" style="width:100%;"/>
+              <br>
+              <img src="{{$image3}}" alt=""/>
               </div>
               <div class="col-xs-15">
                 <i class="fa fa-arrow-down" aria-hidden="true"></i>
-                <img src="{{$image4}}" alt="" style="width:100%;"/>
+                <br>
+                <img src="{{$image4}}" alt=""/>
                 </div>
                 <div class="col-xs-15">
                   <i class="fa fa-arrow-down" aria-hidden="true" style="margin-right:33px;"></i>
-                  <img src="{{$image5}}" alt="" style="width:100%;"/>
+                  <br>
+                  <img src="{{$image5}}" alt=""/>
                   </div>
     </center>
     <div class="table-responsive" style="padding-top:20px;">
